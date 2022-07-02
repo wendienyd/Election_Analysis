0477ffde892d17c9b2cb5d61fd027d51bb793909
Wendy <wrodriguez_16@yahoo.com>
2022-07-01 22:00:11 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -35,75 +35,91 @@ with open(file_to_load) as election_data:
         # 2. Add to the total vote count.
         total_votes += 1
 
-    # Print the candidate name from each row
+        # Print the candidate name from each row
         candidate_name = row[2]
     
-    # If the candidate does not match any existing candidate...
+        # If the candidate does not match any existing candidate...
         if candidate_name not in candidate_options:
             #Add it to the list of candidates.
             candidate_options.append(candidate_name)
 
-    # Begin tracking that candidate's vote count.
+            # Begin tracking that candidate's vote count.
             candidate_votes[candidate_name] = 0
 
-    # Add a vote to that candidate's count.
+        # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
-#Determine the percentage of votes for each candidate by looping through the counts.
-#Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    #Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    #Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    #Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
-
-#To do: print out each candidate's name, vote count, and percentage of votes to terminal.
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-    # Determin winning vote and candidate
-    # Determine if the votes is greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # If true then set winning_count = votes and winning_percent = vote_percentage.
-        winning_count =  votes
-        vinning_percentage = vote_percentage
-        # And, set the winning_candidate equal to the candidate's name.
-        winning_candidate = candidate_name
-
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-winning_candidate_summary = (
-    f"------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-----------------------\n")
-print(winning_candidate_summary)
-
-
-# 3. Print the total votes.
-# print(total_votes)
-
-# #Print the candidate list.
-# print(candidate_options)
-
-# #Print the candidate vote dictionary.
-# print(candidate_votes)
-
-
-
-#Import the datetime class from the datetime module.
-# import datetime as dt
-# #Use the now() attribute on the datetime class to get the present time.
-# now = dt.datetime.now()
-
-#Use the open statement to open the file as a text file.
+#Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
-#Write some data to the file
 
-    #Write three counties to the file.
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+    election_results = (
+        f"\nElection Results\n"
+        f"-----------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-----------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+        
 
-#Open the election results and read the file.
-# with open(file_to_load) as (election_data):
+    #Determine the percentage of votes for each candidate by looping through the counts.
+    #Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        #Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        #Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #Print the candidate name and percentage of votes.
+
+        #To do: print out each candidate's name, vote count, and percentage of votes to terminal.
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+
+        # Determin winning vote and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent = vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    # candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n"))
+
+    winning_candidate_summary = (
+        f"------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-----------------------\n")
+
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
+
+        #print(winning_candidate_summary)
+
+
+        # 3. Print the total votes.
+        # print(total_votes)
+
+        # #Print the candidate list.
+        # print(candidate_options)
+
+        # #Print the candidate vote dictionary.
+        # print(candidate_votes)
+
+
+
+        #Import the datetime class from the datetime module.
+        # import datetime as dt
+        # #Use the now() attribute on the datetime class to get the present time.
+        # now = dt.datetime.now()
+
+    #Use the open statement to open the file as a text file.
+    # with open(file_to_save, "w") as txt_file:
+    # #Write some data to the file
+
+    #     #Write three counties to the file.
+    #     txt_file.write("Arapahoe\nDenver\nJefferson")
+
+    #Open the election results and read the file.
+    # with open(file_to_load) as (election_data):
